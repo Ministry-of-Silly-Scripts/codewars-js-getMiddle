32649e757a8ec5036d3b1ab293f484cf59a64846
Marcio Silva <marcionps@gmail.com>
2023-07-30 20:21:12 -0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 const getMiddle = (s) => {
-  if (s.length % 2 === 1) return s[parseInt(s.length / 2, 10)];
-
   const middle = parseInt(s.length / 2, 10);
-
+  if (s.length % 2 === 1) return s[middle];
   return `${s[middle - 1]}${s[middle]}`;
 };
 
